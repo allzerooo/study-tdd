b2e1d6c54bb1b541900ab1c998affd3e7865ca46
최다영 <dychoi@docfriends.com>
2021-04-08 23:06:19 +0900

    달러 테스트 - amount 비교 -> Dollar 비교로 변경

--- test/com/study/money/DollarTest.java
+++ test/com/study/money/DollarTest.java
@@ -10,7 +10,12 @@ class DollarTest {
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+
+        /*
+            times()는 Dollar를 반환하는데, assertEquals(10, product.amount)는 Dollar를 비교하는 것은 아니기 때문에
+            Dollar와 Dollar를 비교하는 코드로 변경. Dollar에 equals를 재정의했기 때문에 가능.
+         */
+        assertEquals(new Dollar(10), product);
         product = five.times(3);
         assertEquals(15, product.amount);
     }
