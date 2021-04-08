fb932c5dbd07bb75322e3ef75843354db41a6548
ddalam <dychoi.dev@gmail.com>
2021-04-08 23:38:09 +0900

    equals 메서드를 상위 클래스로 옮기기

--- src/com/study/money/Money.java
+++ src/com/study/money/Money.java
@@ -2,6 +2,10 @@ package com.study.money;
 
 public class Money {
 
-    // 하위 클래스에서 사용할 수 있도록 private에서 protected로 변경
     protected int amount;
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
