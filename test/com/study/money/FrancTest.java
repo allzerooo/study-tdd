f652390c0308d6f7467b77ff833a33a558835a98
ddalam <dychoi.dev@gmail.com>
2021-04-08 23:42:18 +0900

    일단 Dollar 테스트 코드를 복사/붙여넣기

--- test/com/study/money/FrancTest.java
+++ test/com/study/money/FrancTest.java
@@ -2,7 +2,7 @@ package com.study.money;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FrancTest {
 
@@ -11,5 +11,11 @@ public class FrancTest {
         Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
+    void testEquality() {
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
