b0795803e9136d93f9203040297d3fc5a9908039
ddalam <dychoi.dev@gmail.com>
2021-04-09 00:01:06 +0900

    times 메서드를 상위 클래스로 옮기기 - 팩토리 메서드를 테스트 코드에 적용

--- test/com/study/money/DollarTest.java
+++ test/com/study/money/DollarTest.java
@@ -9,13 +9,13 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
     }
 }
