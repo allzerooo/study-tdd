a8b9132c89e9610f5b7bba1ce54238223ebb2e48
ddalam <dychoi.dev@gmail.com>
2021-04-08 23:28:35 +0900

    중복을 많이 만들었지만, 일단 돌아가도록 새로운 객체를 복/붙으로 생성

--- test/com/study/money/DollarTest.java
+++ test/com/study/money/DollarTest.java
@@ -9,8 +9,6 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-
-        // Dollar의 equals가 정확하게 정확히 작동하지 않으면 -> times 역시 정확하게 작동한다는 것을 검증하기 어렵다
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
@@ -19,5 +17,12 @@ class DollarTest {
     void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+
+    @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 }
