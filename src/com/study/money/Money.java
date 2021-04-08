f942b70c4d67a111842ad98d340b85020ed3fb0e
ddalam <dychoi.dev@gmail.com>
2021-04-08 23:57:52 +0900

    times 메서드를 상위 클래스로 옮기기 - Money 클래스를 추상 클래스로 변경하기

--- src/com/study/money/Money.java
+++ src/com/study/money/Money.java
@@ -1,9 +1,12 @@
 package com.study.money;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
 
+    abstract Money times(int multiplier);
+
+    // 팩토리 메서드
     static Dollar dollar(int amount) {
         return new Dollar(amount);
     }
@@ -12,4 +15,5 @@ public class Money {
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
 }
