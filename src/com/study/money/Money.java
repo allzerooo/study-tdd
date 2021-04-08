41243cdeaf40df660ad159df36515a8f5804e1ef
ddalam <dychoi.dev@gmail.com>
2021-04-09 00:05:35 +0900

    times 메서드를 상위 클래스로 옮기기 - Franc에 Dollar와 같은 과정 적용하기

--- src/com/study/money/Money.java
+++ src/com/study/money/Money.java
@@ -1,15 +1,17 @@
 package com.study.money;
 
-// Money 클래스에 times()를 구현할 준비가 되지 않았기 때문에 Money클래스를 추상 클래스로 변경 후 Money.times()를 선언
 public abstract class Money {
 
     protected int amount;
 
     abstract Money times(int multiplier);
 
-    // 팩토리 메서드
     static Money dollar(int amount) {
         return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 
     public boolean equals(Object object) {
