3930bf3861dc4c06b38086249f00773ddecf90ac
ddalam <dychoi.dev@gmail.com>
2021-04-12 21:01:07 +0900

    통화 개념 구현하기 - 두 하위 클래스에 동일한 기능 추가

---
+++ test/com/study/money/MoneyTest.java
@@ -0,0 +1,14 @@
+package com.study.money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+
+    @Test
+    void testCurrency() {
+        assertEquals("USD", Money.dollar(1).currency());
+        assertEquals("CHF", Money.franc(1).currency());
+    }
+}
