0dcb7bc0106aa01f908ca6a723546090b6d819de
ddalam <dychoi.dev@gmail.com>
2021-04-30 22:18:40 +0900

    Bank.reduce 인자로 Money를 넘겼을 경우 테스트

--- src/com/study/money/Bank.java
+++ src/com/study/money/Bank.java
@@ -3,6 +3,8 @@ package com.study.money;
 public class Bank {
 
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        if (source instanceof Money) return (Money) source;
+        Sum sum = (Sum) source;
+        return sum.reduce(to);
     }
 }
